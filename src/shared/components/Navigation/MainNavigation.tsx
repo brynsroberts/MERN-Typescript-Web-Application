7e89e4f30eae1cd463996183e672d794c833bf7b
brynsroberts <bryan.roberts.home@gmail.com>
2022-01-15 10:47:53 -0800

    add menu links and styles

--- src/shared/components/Navigation/MainNavigation.tsx
+++ src/shared/components/Navigation/MainNavigation.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
 import { Link } from "react-router-dom";
 import "./MainNavigation.css";
 
 const MainNavigation: React.FC = () => {
   return (
     <MainHeader>
-      <button className="main-navigation__menu_btn">
+      <button className="main-navigation__menu-btn">
         <span />
         <span />
         <span />
@@ -15,7 +16,9 @@ const MainNavigation: React.FC = () => {
       <h1 className="main-navigation__title">
         <Link to="/">App Name</Link>
       </h1>
-      <nav>...</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
   );
 };
