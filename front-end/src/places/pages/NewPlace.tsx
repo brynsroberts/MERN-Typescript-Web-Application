243241a7bcdcdd1e4e17092a1650e222f940eac7
brynsroberts <bryan.roberts.home@gmail.com>
2022-01-22 08:55:13 -0800

    add validation to new place page

--- front-end/src/places/pages/NewPlace.tsx
+++ front-end/src/places/pages/NewPlace.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 
 import Input from "../../shared/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 import "./NewPlace.css";
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input type="text" label="Title" element="input" id="1" />
+      <Input
+        type="text"
+        label="Title"
+        element="input"
+        id="1"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+      />
     </form>
   );
 };
--- front-end/src/shared/FormElements/Input.tsx
+++ front-end/src/shared/FormElements/Input.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useReducer } from "react";
 
+import { validate } from "../util/validators";
 import "./Input.css";
 
 interface InputProps {
@@ -9,19 +10,73 @@ interface InputProps {
   placeholder?: string;
   rows?: number;
   label: string;
+  validators?: any[];
+  onInput?: () => any;
+  errorText?: string;
 }
 
+const inputReducer = (
+  state: {
+    value: string;
+    isValid: any;
+  },
+  action: {
+    type?: "CHANGE";
+    val: string;
+    validators: any;
+  }
+) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validators),
+      };
+    default:
+      return state;
+  }
+};
+
 const Input: React.FC<InputProps> = (props) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: "",
+    isValid: false,
+  });
+  const changeHandler = (event: any) => {
+    dispatch({
+      type: "CHANGE",
+      val: event.target.value,
+      validators: props.validators,
+    });
+  };
   const element =
     props.element === "input" ? (
-      <input id={props.id} type={props.type} placeholder={props.placeholder} />
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={changeHandler}
+        value={inputState.value}
+      />
     ) : (
-      <textarea id={props.id} rows={props.rows || 3} />
+      <textarea
+        id={props.id}
+        rows={props.rows || 3}
+        onChange={changeHandler}
+        value={inputState.value}
+      />
     );
+
   return (
-    <div className={`form-control`}>
+    <div
+      className={`form-control ${
+        !inputState.isValid && "form-control--invalid"
+      }`}
+    >
       <label htmlFor={props.id}>{props.label}</label>
       {element}
+      {!inputState.isValid && <p>{props.errorText}</p>}
     </div>
   );
 };
